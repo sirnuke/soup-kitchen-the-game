fb2f88686c97a2ac9d2a8d460ec30fded7ebce31
Bryan DeGrendel <bryan@degrendel.com>
2014-03-22 12:50:56 -0400

    Added real basic Love structure.
    1. Fixed love.load problems, as callbacks are just conventions.

---
+++ soup-kitchen/mainmenu.lua
@@ -0,0 +1,17 @@
+-- Soup Kitchen
+-- Bryan DeGrendel (c) 2014
+
+mainmenu = {}
+
+function mainmenu.enter()
+end
+
+function mainmenu.exit()
+end
+
+function mainmenu.draw()
+end
+
+function mainmenu.update(dt)
+end
+
