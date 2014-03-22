f177eb8f808cf94d14eb206edb3dd6ecceedcf2d
Bryan DeGrendel <bryan@degrendel.com>
2014-03-22 00:54:06 -0400

    Added empty class Player.

---
+++ include/player.h
@@ -0,0 +1,15 @@
+// Soup Kitchen
+// Bryan DeGrendel (c) 2014
+
+#ifndef _PLAYER_H_
+#define _PLAYER_H_
+
+class Player
+{
+public:
+  Player();
+  ~Player();
+private:
+};
+
+#endif//_PLAYER_H_
---
+++ source/player.cpp
@@ -0,0 +1,13 @@
+// Soup Kitchen
+// Bryan DeGrendel (c) 2014
+
+#include "player.h"
+
+Player::Player()
+{
+}
+
+Player::~Player()
+{
+}
+
