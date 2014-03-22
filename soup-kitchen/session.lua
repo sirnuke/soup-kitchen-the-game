4eca6c0eb2344021763aa21c0acd0d103b4bb8ec
Bryan DeGrendel <bryan@degrendel.com>
2014-03-22 17:46:12 -0400

    Session now tracks the player pawn, plus cash

--- soup-kitchen/session.lua
+++ soup-kitchen/session.lua
@@ -6,11 +6,14 @@ session = {}
 function session.start()
   session.day = 1
   session.time = 0.0
+  session.cash = 1000
   session.stock = {}
   for i = 1,3 do table.insert(session.stock, StockClass.random('core')) end
   for i = 1,5 do table.insert(session.stock, StockClass.random('side')) end
   for i = 1,3 do table.insert(session.stock, StockClass.random('drink')) end
   for i = 1,5 do table.insert(session.stock, StockClass.random('desert')) end
   for i = 1,2 do table.insert(session.stock, StockClass.random('salad')) end
+
+  session.player = PawnClass.new('player', 5, 11)
 end
 
