e922819ae455e7db658f6d575ac6543ee5dd3ae5
Bryan DeGrendel <bryan@degrendel.com>
2014-03-22 00:37:58 -0400

    Main now makes a window.

--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,25 @@
 // Soup Kitchen
 // Bryan DeGrendel (c) 2014
 
+#include <SFML/Graphics.hpp>
+
 int main(int argc, char *argv[])
 {
+
+  sf::RenderWindow window(sf::VideoMode(200, 200), "Soup Kitchen");
+
+  sf::Event event;
+  while (window.isOpen())
+  {
+    while (window.pollEvent(event))
+    {
+      if (event.type == sf::Event::Closed)
+        window.close();
+    }
+
+    window.clear();
+    window.display();
+  }
   return 0;
 }
 
