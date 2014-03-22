1fdc05cfee38dda54380bd1d9b66d62b35624473
Bryan DeGrendel <bryan@degrendel.com>
2014-03-22 17:02:25 -0400

    Session now seeds the initial food stock

--- soup-kitchen/session.lua
+++ soup-kitchen/session.lua
@@ -7,6 +7,10 @@ function session.start()
   session.day = 1
   session.time = 0.0
   session.stock = {}
-  table.insert(session.stock, StockClass.new(0, 0))
+  for i = 1,3 do table.insert(session.stock, StockClass.random('core')) end
+  for i = 1,5 do table.insert(session.stock, StockClass.random('side')) end
+  for i = 1,3 do table.insert(session.stock, StockClass.random('drink')) end
+  for i = 1,5 do table.insert(session.stock, StockClass.random('desert')) end
+  for i = 1,2 do table.insert(session.stock, StockClass.random('salad')) end
 end
 
