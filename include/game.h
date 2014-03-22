cc5dcb97eed330a7a521cc28fb024c42e048078b
Bryan DeGrendel <bryan@degrendel.com>
2014-03-21 23:24:24 -0400

    Added empty class wrapping around a game instance.

---
+++ include/game.h
@@ -0,0 +1,15 @@
+// Soup Kitchen
+// Bryan DeGrendel (c) 2014
+
+#ifndef _GAME_H_
+#define _GAME_H_
+
+class Game
+{
+public:
+  Game();
+  ~Game();
+private:
+};
+
+#endif//_GAME_H_
---
+++ source/game.cpp
@@ -0,0 +1,13 @@
+// Soup Kitchen
+// Bryan DeGrendel (c) 2014
+
+#include "game.h"
+
+Game::Game()
+{
+}
+
+Game::~Game()
+{
+}
+
