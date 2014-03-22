b2fb2e0a057956ae87819e1baa2c61c33b92b6e8
Bryan DeGrendel <bryan@degrendel.com>
2014-03-22 17:02:16 -0400

    Further fleshed out the stock class

--- soup-kitchen/stock.lua
+++ soup-kitchen/stock.lua
@@ -3,19 +3,77 @@
 
 StockClass = {}
 StockClass.__index = StockClass
-StockClass.types = { 'core', 'side', 'drink', 'desert' }
+StockClass.types = { core='core', side='side', drink='drink', desert='desert', salad='salad' }
 
-function StockClass.new(type, expiration)
+function StockClass.new(type, quantity, expiration)
+  assert(StockClass.types[type])
+  assert(expiration > 0)
+  assert(quantity > 0)
+
   local instance = {}
   setmetatable(instance, StockClass)
   instance.type = type
   instance.expiration = expiration
+  instance.quantity = quantity
   instance.prepared = false
   return instance
 end
 
 function StockClass:__tostring()
-  return string.format("Type:%i Expr:%i Prep:%s", self.type, self.expiration,
-    tostring(self.prepared))
+  return string.format("%i of %s (prep:%s) expires in %i", self.quantity,
+    StockClass.types[self.type], tostring(self.prepared), self.expiration)
+end
+
+function StockClass:expire()
+  self.expired = self.expired - 1
+  if self.expired <= 0 then
+    return true
+  else
+    return false
+  end
+end
+
+function StockClass:use(amount)
+  self.quantity = self.quantity - amount
+  if self.quantity <= 0 then
+    return true
+  else
+    return false
+  end
+end
+
+function StockClass.random(type)
+  assert(StockClass.types[type])
+
+  local quantity, expiration = 0, 0
+
+  if type == 'core' then
+    quantity = love.math.randomNormal(5, 20)
+    expiration = love.math.randomNormal(2, 7)
+  elseif type == 'side' then
+    quantity = love.math.randomNormal(5, 20)
+    expiration = love.math.randomNormal(3, 10)
+  elseif type == 'drink' then
+    quantity = love.math.randomNormal(10, 30)
+    expiration = love.math.randomNormal(2, 7)
+  elseif type == 'desert' then
+    quantity = love.math.randomNormal(3, 15)
+    expiration = love.math.randomNormal(1, 3)
+  elseif type == 'salad' then
+    quantity = love.math.randomNormal(10, 30)
+    expiration = love.math.randomNormal(2, 6)
+  else
+    assert(false, string.format("Unhandled type %s", type))
+  end
+
+  if quantity <= 0 then
+    print(string.format("Generated bad quantity of %i", quantity))
+    quantity = 1
+  end
+  if expiration <= 0 then
+    print(string.format("Generated bad expiration of %i", expiration))
+    expiration = 1
+  end
+  return StockClass.new(type, quantity, expiration)
 end
 
