566b08cc0b0c52bbe8c2ad7b68655549378c224f
Bryan DeGrendel <bryan@degrendel.com>
2014-03-22 17:12:05 -0400

    Updated quit key and reset color on every draw

--- soup-kitchen/main.lua
+++ soup-kitchen/main.lua
@@ -22,11 +22,12 @@ function love.load()
 end
 
 function love.draw()
+  love.graphics.setColor(255, 255, 255, 255)
   core.scene.draw()
 end
 
 function love.keypressed(key)
-  if key == "`" then
+  if key == "q" then
     love.event.quit()
   else
     core.scene.keypressed(key)
