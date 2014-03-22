650c5629e8048132d261b6d49f149442eeab4e8b
Bryan DeGrendel <bryan@degrendel.com>
2014-03-22 16:33:02 -0400

    Further fleshed out the stock class

--- soup-kitchen/stock.lua
+++ soup-kitchen/stock.lua
@@ -3,16 +3,19 @@
 
 StockClass = {}
 StockClass.__index = StockClass
+StockClass.types = { 'core', 'side', 'drink', 'desert' }
 
 function StockClass.new(type, expiration)
   local instance = {}
   setmetatable(instance, StockClass)
   instance.type = type
   instance.expiration = expiration
+  instance.prepared = false
   return instance
 end
 
 function StockClass:__tostring()
-  return string.format("Type:%i Expr:%i", self.type, self.expiration)
+  return string.format("Type:%i Expr:%i Prep:%s", self.type, self.expiration,
+    tostring(self.prepared))
 end
 
