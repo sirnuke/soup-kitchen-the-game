a9f5427f4c0368e241831c292ad2ac4820fa5dc0
Bryan DeGrendel <bryan@degrendel.com>
2014-03-22 21:46:34 -0400

    Added various constants

--- soup-kitchen/config.lua
+++ soup-kitchen/config.lua
@@ -6,6 +6,10 @@ function core.config()
   core.constants = {}
   core.constants.eat = 0.25
   core.constants.walk = 100
+  core.constants.font_size = 22
+  core.constants.day_start = 450 -- 7:30am
+  core.constants.day_end = 1170 -- 7:30pm
+  core.constants.money_start = 1000
   core.sizes = {}
   core.sizes.square = {width=64, height=64}
   core.sizes.pawn = {width=32, height=32}
