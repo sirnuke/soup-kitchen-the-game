061537231431159cdd71f5e7a999f1a9c0cf2703
Bryan DeGrendel <bryan@degrendel.com>
2014-03-22 23:55:59 -0400

    Added an empty customer class

--- soup-kitchen/customer.lua
+++ soup-kitchen/customer.lua
@@ -1,3 +1,12 @@
 -- Soup Kitchen
 -- Bryan DeGrendel (c) 2014
 
+CustomerClass = {}
+CustomerClass.__index = CustomerClass
+
+function CustomerClass.new()
+  local instance = {}
+  setmetatable(instance, CustomerClass)
+  return instance
+end
+
