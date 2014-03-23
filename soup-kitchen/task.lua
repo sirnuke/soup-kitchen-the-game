f430418adc3114ec5162f45789f7ae9d69646e74
Bryan DeGrendel <bryan@degrendel.com>
2014-03-23 02:15:56 -0400

    Added description to tasks

--- soup-kitchen/task.lua
+++ soup-kitchen/task.lua
@@ -5,12 +5,13 @@ TaskClass = {}
 TaskClass.__index = TaskClass
 TaskClass.types = { serving='serving' }
 
-function TaskClass.new(type)
+function TaskClass.new(type, description)
   assert(TaskClass.types[type])
 
   local instance = {}
   setmetatable(instance, TaskClass)
   instance.type = type
+  instance.description = description
   return instance
 end
 
