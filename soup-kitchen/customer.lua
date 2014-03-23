a238ae4753df09805f35c78edbb1ae021cb98f5b
Bryan DeGrendel <bryan@degrendel.com>
2014-03-23 01:13:57 -0400

    Customer now tracks and creates a pawn

--- soup-kitchen/customer.lua
+++ soup-kitchen/customer.lua
@@ -7,6 +7,30 @@ CustomerClass.__index = CustomerClass
 function CustomerClass.new()
   local instance = {}
   setmetatable(instance, CustomerClass)
+  instance.pawn = nil
   return instance
+end
+
+function CustomerClass:place()
+  self.pawn = PawnClass.new('customer', core.constants.entry_location.x,
+    core.constants.entry_location.y)
+end
+
+function CustomerClass:spawned()
+  if self.pawn then
+    return true
+  else
+    return false
+  end
+end
+
+function CustomerClass:update(dt)
+  instance.pawn:update(dt)
+end
+
+function CustomerClass:draw()
+  if self.pawn then
+    self.pawn:draw()
+  end
 end
 
