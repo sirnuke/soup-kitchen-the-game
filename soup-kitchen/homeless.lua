bcae64f589d5e20f642b08f14a76dafda0626e8a
Bryan DeGrendel <bryan@degrendel.com>
2014-03-23 00:02:49 -0400

    Homeless now returns an integer spawn count
    1. Also implemented a basic completed function.

--- soup-kitchen/homeless.lua
+++ soup-kitchen/homeless.lua
@@ -7,8 +7,12 @@ function homeless.spawn()
   if not homeless.demand then
     homeless.demand = core.constants.homeless_need
   end
+  return math.floor(love.math.randomNormal(10, homeless.demand))
 end
 
 function homeless.complete(served)
+  local delta = homeless.demand - served
+  homeless.demand = homeless.demand + love.math.randomNormal(delta / 5, delta)
+  print("New demand is", homeless.demand)
 end
 
