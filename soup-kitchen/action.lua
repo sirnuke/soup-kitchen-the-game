b53111a7ed926ac740fd016d431cd11914ec6849
Bryan DeGrendel <bryan@degrendel.com>
2014-03-23 00:44:42 -0400

    Implemented next function for action

--- soup-kitchen/action.lua
+++ soup-kitchen/action.lua
@@ -26,3 +26,39 @@ function ActionClass.new(type, customer, volunteer)
   return instance
 end
 
+function ActionClass:next(stage, current)
+  if current.type == 'drinks' then
+    return map.actions.food1
+  elseif current.type == 'food1' then
+    return map.actions.food2
+  elseif current.type == 'food2' then
+    if stage == 'breakfast' then
+      return nil
+    else
+      return map.actions.food3
+    end
+  elseif current.type == 'food3' then
+    return map.actions.food4
+  elseif current.type == 'food4' then
+    return map.actions.cleaning1
+  elseif current.type == 'cleaning1' then
+    return map.actions.cleaning2
+  elseif current.type == 'prepare1' then
+    return map.actions.storage1
+  elseif current.type == 'prepare2' then
+    return map.actions.storage1
+  elseif current.type == 'prepare3' then
+    return map.actions.storage2
+  elseif current.type == 'prepare4' then
+    return map.actions.storage2
+  elseif current.type == 'prepare5' then
+    return map.actions.storage3
+  elseif current.type == 'prepare6' then
+    return map.actions.storage3
+  elseif current.type == 'trash' then
+    return nil
+  else
+    assert(false, string.format("Unhandled type of %s", current.type))
+  end
+end
+
