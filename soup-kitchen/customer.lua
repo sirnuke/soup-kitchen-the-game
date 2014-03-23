0f50796164620787819895b98e82326b19e6ff26
Bryan DeGrendel <bryan@degrendel.com>
2014-03-23 17:50:38 -0400

    Start of a better structure of customer

--- soup-kitchen/customer.lua
+++ soup-kitchen/customer.lua
@@ -3,56 +3,41 @@
 
 CustomerClass = {}
 CustomerClass.__index = CustomerClass
+CustomerClass.states = { waiting='waiting', inline='inline', gotfood='gotfood', eating='eating' }
 
 function CustomerClass.new()
   local instance = {}
   setmetatable(instance, CustomerClass)
   instance.pawn = nil
   instance.action = nil
-  instance.complete = 0
+  instance.state = nil
   return instance
 end
 
-function CustomerClass:place()
-  self.pawn = PawnClass.new('customer', core.constants.entry_location.x,
-    core.constants.entry_location.y)
-  self.action = map.actions.drinks
-end
-
-function CustomerClass:spawned()
-  if self.pawn then
-    return true
-  else
-    return false
-  end
+function CustomerClass:spawn()
+  self.state = 'waiting'
 end
 
 function CustomerClass:update(dt)
-  if not self.pawn then
-    return
-  end
-  self.pawn:update(dt)
-  if not self.action then
-    return
-  end
-  if self.pawn.coordinate.x ~= self.action.customer.x 
-    or self.pawn.coordinate.y ~= self.action.customer.y then
-    if self.pawn:arrived() then
-      if not map.occupant(self.action.customer.x, self.action.customer.y) then
-        print("Go to", self.action.customer.x, self.action.customer.y)
-        self.pawn:go(self.action.customer.x, self.action.customer.y)
-        self.action:reset()
-      end
+  if not self.state then return end
+
+  if self.state == 'waiting' then
+    if not map.occupant(core.constants.entry_location.x, core.constants.entry_location.y) then
+      self.pawn = PawnClass.new('customer', core.constants.entry_location.x,
+        core.constants.entry_location.y)
+      self.action = map.actions.drinks
+      self.state = 'inline'
     end
+  elseif self.state == 'inline' then
+    self.pawn:update(dt)
+    if self.pawn.coordinate.x ~= self.action.customer.x 
+      or self.pawn.coordinate.y ~= self.action.customer.y then
+    end
+  elseif self.state == 'gotfood' then
+    self.pawn:update(dt)
+  elseif self.state == 'eating' then
   else
-    self.action:update(dt)
-    if self.action:finished() then
-      self.action = self.action:next(session.stage, self.action)
-      if self.action == 'exit' then
-        self.pawn:go(core.constants.exit_location.x, core.constants.exit_location.y)
-        self.action = nil
-      end
-    end
+    assert(false, string.format("Unknown customer state of %s", self.state))
   end
 end
 
