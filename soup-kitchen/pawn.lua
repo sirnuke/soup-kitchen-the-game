65fc46801c504a4a813ddb80eaa9290c5a44b4ca
Bryan DeGrendel <bryan@degrendel.com>
2014-03-22 20:41:23 -0400

    Fixed pathfinding

--- soup-kitchen/pawn.lua
+++ soup-kitchen/pawn.lua
@@ -15,7 +15,6 @@ function PawnClass.new(type, x, y)
   if not PawnClass.pathfinding then
     PawnClass.pathfinding = { }
     PawnClass.pathfinding.map = { }
-    PawnClass.pathfinding.neighbors = { }
 
     for y = 1,core.sizes.map.height do
       local row = { }
@@ -24,12 +23,6 @@ function PawnClass.new(type, x, y)
       end
       table.insert(PawnClass.pathfinding.map, row)
     end
-
-    for x = -1,1 do for y = -1,1 do
-      if x ~= 0 or y ~= 0 then
-        table.insert(PawnClass.pathfinding.neighbors, { x=x, y=y } )
-      end 
-    end end
   end
 
   local instance = {}
@@ -40,6 +33,7 @@ function PawnClass.new(type, x, y)
   instance.screen.x = instance.position.x - (core.sizes.square.width - core.sizes.pawn.width) / 2
   instance.screen.y = instance.position.y - (core.sizes.square.height - core.sizes.pawn.height) / 2
   instance.coordinate = { x=x, y=y }
+  instance.path = nil
   instance.destination = nil
   instance.skills = {}
 
@@ -71,33 +65,36 @@ function PawnClass:go(x, y)
 
   print("Performing breadth first search...")
   local queue = { self.coordinate }
-  local nx, ny
 
   while #queue > 0 and (queue[1].x ~= x or queue[1].y ~= y) do
     print("Getting neighbors of ", queue[1].x, queue[1].y)
-
-    for k,v in ipairs(PawnClass.pathfinding.neighbors) do
-      nx, ny = queue[1].x + v.x, queue[1].y + v.y
-      if map.validcoordinate(nx, ny) and not PawnClass.pathfinding.map[ny][nx] then
-        PawnClass.pathfinding.map[ny][nx] = { x=queue[1].x, y=queue[1].y }
-        table.insert(queue, { x=nx, y=ny })
+    for k,v in ipairs(map.getneighbors(queue[1].x, queue[1].y)) do
+      if not PawnClass.pathfinding.map[v.y][v.x] then
+        PawnClass.pathfinding.map[v.y][v.x] = { x=queue[1].x, y=queue[1].y }
+        table.insert(queue, { x=v.x, y=v.y })
       end
     end
     table.remove(queue, 1)
   end
-  assert(queue[1].x == x and queue[1].y == y, "Couldn't find a path!")
-  self.destination = { {x=x, y=y} }
+  assert(#queue > 0, "Couldn't find a path!")
+  assert(queue[1].x == x and queue[1].y == y, "Top of queue should be the end")
+  self.path = { {x=x, y=y} }
   local square = PawnClass.pathfinding.map[y][x]
   print("Path ends up at ", x, y, type(square))
   while type(square) ~= "boolean" do
     assert(type(square) == "table")
-    table.insert(self.destination, { x=square.x, y=square.y })
+    table.insert(self.path, { x=square.x, y=square.y })
     print("Looking up parent of ", square.x, square.y)
     square = PawnClass.pathfinding.map[square.y][square.x]
   end
   for y = 1,core.sizes.map.height do for x = 1,core.sizes.map.width do
     PawnClass.pathfinding.map[y][x] = nil
   end end
+  print("Removing", self.path[#self.path].x, self.path[#self.path].y)
+  table.remove(self.path)
+  print("Next step is", self.path[#self.path].x, self.path[#self.path].y)
+
+  self.destination = map.position(self.path[#self.path].x, self.path[#self.path].y)
 
   map.setoccupant(self.coordinate.x, self.coordinate.y, nil)
   map.setoccupant(x, y, self)
@@ -106,9 +103,14 @@ function PawnClass:go(x, y)
 end
 
 function PawnClass:update(dt)
-  if self.destination then
-    --self.position = self.destination
-    self.destination = nil
+  if self.path then
+    assert(#self.path > 0 and self.destination)
+    local dx, dy = self.destination.x - self.position.x, self.destination.y - self.position.y
+    print("Current is", self.position.x, self.position.y)
+    print("Destination is", self.destination.x, self.destination.y)
+    assert(dx == 0 or dy == 0)
+    dx, dy = dx * core.constants.walk, dy * core.constants.walk
+    print("Would move", dx, dy)
   end
 end
 
