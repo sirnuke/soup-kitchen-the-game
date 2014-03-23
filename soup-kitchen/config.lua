50e367ccc213261afe3eedac94bb5565d7f491e5
Bryan DeGrendel <bryan@degrendel.com>
2014-03-22 22:29:45 -0400

    Added constants for employee wage and time scale

--- soup-kitchen/config.lua
+++ soup-kitchen/config.lua
@@ -7,10 +7,12 @@ function core.config()
   core.constants.eat = 0.25
   core.constants.walk = 100
   core.constants.font_size = 22
+  core.constants.time_scale = 0.5
   core.constants.small_font_size = 14
   core.constants.day_start = 450 -- 7:30am
   core.constants.day_end = 1170 -- 7:30pm
   core.constants.money_start = 1000
+  core.constants.employee_wage = 50
   core.sizes = {}
   core.sizes.square = {width=64, height=64}
   core.sizes.pawn = {width=32, height=32}
