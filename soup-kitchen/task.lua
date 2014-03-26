c1a657713c7cf969fdf869dae929aeaac64f84a8
Bryan DeGrendel <bryan@degrendel.com>
2014-03-25 23:02:48 -0400

    First steps of a better Task class

--- soup-kitchen/task.lua
+++ soup-kitchen/task.lua
@@ -3,15 +3,18 @@
 
 TaskClass = {}
 TaskClass.__index = TaskClass
-TaskClass.types = { serving='serving' }
+TaskClass.types = { serving='serving', stock='stocking' }
 
-function TaskClass.new(type, description)
+function TaskClass.new(type, description, locations)
   assert(TaskClass.types[type])
+  assert(description)
+  assert(locations)
 
   local instance = {}
   setmetatable(instance, TaskClass)
   instance.type = type
   instance.description = description
+  instance.locations = locations
   return instance
 end
 
