48dc11b22da62f47d7912058af834c5eb444920f
Bryan DeGrendel <bryan@degrendel.com>
2014-03-26 00:33:12 -0400

    Start of cleanup/refactoring of Serving class

--- soup-kitchen/serving.lua
+++ soup-kitchen/serving.lua
@@ -27,13 +27,13 @@ function ServingClass.new(id, location, customer, volunteer)
   instance.stage = nil
   instance.stock_source = nil
   instance.quantity = nil
-  instance.progress = 0
+  --instance.progress = 0
 
-  instance.state = 'inactive'
+  --instance.state = 'inactive'
 
-  instance.tasks = {}
-  instance.tasks.stocking = 'inactive'
-  instance.tasks.serving = 'inactive'
+  --instance.tasks = {}
+  --instance.tasks.stocking = 'inactive'
+  --instance.tasks.serving = 'inactive'
 
   return instance
 end
@@ -42,15 +42,13 @@ function ServingClass:screen(coord)
   return { x= (coord.x - 1) * constants.sizes.square, y= (coord.y - 1) * constants.sizes.square }
 end
 
-function ServingClass:new_stage(stage) 
-  assert(stage)
-  self.stage = stage
-end
+--function ServingClass:new_stage(stage) 
+  --assert(stage)
+  --self.stage = stage
+--end
 
-function ServingClass:stock(quantity)
-  if self.quantity > 0 then
-    session.add_trash(self.quantity * constants.trash.food_scale)
-  end
+function ServingClass:set_stock(stock_source, quantity)
+  self.stock_source = stock_source
   self.quantity = quantity
 end
 
@@ -58,62 +56,60 @@ function ServingClass:reset()
   self.progress = 0
 end
 
-function ServingClass:update(dt, line)
-  local customer, volunteer = map.occupant(self.customer), map.occupant(self.volunteer)
+--function ServingClass:update(dt, line)
+  --local customer, volunteer = map.occupant(self.customer), map.occupant(self.volunteer)
 
-  if self.id > constants.breakfast_end 
-    and (self.stage == 'start' or self.stage == 'breakfast') then
-    self.tasks.stocking = 'inactive'
-    self.tasks.serving = 'inactive'
-    return
-  end
+  --if self.id > constants.breakfast_end 
+    --and (self.stage == 'start' or self.stage == 'breakfast') then
+    --self.tasks.stocking = 'inactive'
+    --self.tasks.serving = 'inactive'
+    --return
+  --end
 
-  if not self.stock_source or self.stage == 'cleanup' or self.stage == 'done' then
-    self.tasks.stocking = 'inactive'
-  else
-    if self.quantity >= constants.stock.serve[self.stock_source.type] * .67 then
-      self.tasks.stocking = 'full'
-    elseif self.quantity >= constants.stock.server[self.stock_source.type] * .33 then
-      self.tasks.stocking = 'medium'
-    elseif self.quantity > 0 then
-      self.tasks.stocking = 'low'
-    else
-      self.tasks.stocking = 'empty'
-    end
-  end
+  --if not self.stock_source or self.stage == 'cleanup' or self.stage == 'done' then
+    --self.tasks.stocking = 'inactive'
+  --else
+    --if self.quantity >= constants.stock.serve[self.stock_source.type] * .67 then
+      --self.tasks.stocking = 'full'
+    --elseif self.quantity >= constants.stock.server[self.stock_source.type] * .33 then
+      --self.tasks.stocking = 'medium'
+    --elseif self.quantity > 0 then
+      --self.tasks.stocking = 'low'
+    --else
+      --self.tasks.stocking = 'empty'
+    --end
+  --end
 
-  if not line then
-    self.tasks.serving = 'inactive'
-    return
-  end
+  --if not line then
+    --self.tasks.serving = 'inactive'
+    --return
+  --end
 
-  if not customer or customer.action ~= self then
-    self.tasks.serving = 'potential'
-    return
-  end
+  --if not customer or customer.action ~= self then
+    --self.tasks.serving = 'potential'
+    --return
+  --end
 
-  if not volunteer or not volunteer:arrived() then
-    table.insert(session.tasks, TaskClass.new('serving', 
-    string.format("Need help at (%i,%i)", self.volunteer.x, self.volunteer.y)))
-    self.tasks.serving = 'halted'
-  elseif customer:arrived() then
-    self.tasks.serving = 'active'
-    self.progress = self.progress + dt * constants.scale.work
-  end
-end
+  --if not volunteer or not volunteer:arrived() then
+    --table.insert(session.tasks, TaskClass.new('serving', 
+    --string.format("Need help at (%i,%i)", self.volunteer.x, self.volunteer.y)))
+    --self.tasks.serving = 'halted'
+  --elseif customer:arrived() then
+    --self.tasks.serving = 'active'
+    --self.progress = self.progress + dt * constants.scale.work
+  --end
+--end
 
-function ServingClass:done()
-  if self.progress >= constants.max_progress then
-    return true
-  else
-    return false
-  end
-end
+--function ServingClass:done()
+  --if self.progress >= constants.max_progress then
+    --return true
+  --else
+    --return false
+  --end
+--end
 
-function ServingClass:next()
-  assert(self.next_stage)
-  assert(self.stage)
-  if self.id >= constants.breakfast_end and self.stage == 'breakfast' then
+function ServingClass:next(stage)
+  if self.id >= constants.breakfast_end and stage == 'breakfast' then
     return 'done'
   end
   return self.next_stage
@@ -121,27 +117,28 @@ end
 
 function ServingClass:draw()
   local image = nil
-  assert(ServingClass.serving_states[self.tasks.serving])
-  assert(ServingClass.stocking_states[self.tasks.stocking])
+  -- TODO: Draw quantity level image
+  --assert(ServingClass.serving_states[self.tasks.serving])
+  --assert(ServingClass.stocking_states[self.tasks.stocking])
 
-  if self.tasks.stocking ~= 'inactive' then
-    image = ActionClass.images.potential
-  end
-  if image then
-    love.graphics.draw(image, self.screen_volunteer.x, self.screen_volunteer.y)
-  end
+  --if self.tasks.stocking ~= 'inactive' then
+    --image = ActionClass.images.potential
+  --end
+  --if image then
+    --love.graphics.draw(image, self.screen_volunteer.x, self.screen_volunteer.y)
+  --end
 
-  image = nil
-  if self.tasks.serving == 'potential' then
-    image = ActionClass.images.potential
-  elseif self.tasks.serving == 'active' then
-    image = ActionClass.images.active
-  elseif self.tasks.serving == 'halted' then
-    image = ActionClass.images.halted
-  end
-  if image then 
-    love.graphics.draw(image, self.screen_customer.x, self.screen_customer.y)
-    love.graphics.draw(image, self.screen_volunteer.x, self.screen_volunteer.y)
-  end
+  --image = nil
+  --if self.tasks.serving == 'potential' then
+    --image = ActionClass.images.potential
+  --elseif self.tasks.serving == 'active' then
+    --image = ActionClass.images.active
+  --elseif self.tasks.serving == 'halted' then
+    --image = ActionClass.images.halted
+  --end
+  --if image then 
+    --love.graphics.draw(image, self.screen_customer.x, self.screen_customer.y)
+    --love.graphics.draw(image, self.screen_volunteer.x, self.screen_volunteer.y)
+  --end
 end
 
