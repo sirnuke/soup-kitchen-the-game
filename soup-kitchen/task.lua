e155a387f2a94559f7e135e1777672fbf7820f5d
Bryan DeGrendel <bryan@degrendel.com>
2014-03-28 23:18:59 -0400

    Better Task behavior
    1. Tasks now update themselves, and return true for done() when
    completed.

--- soup-kitchen/task.lua
+++ soup-kitchen/task.lua
@@ -13,14 +13,39 @@ function TaskClass.new(type, locations)
   setmetatable(instance, TaskClass)
   instance.type = type
   instance.locations = locations
+  instance.progress = 0
+  if type == 'serving' then
+    assert(#locations == 2)
+  elseif type == 'stocking' then
+  else
+    assert(false, string.format("Unhandled task type of %s", type))
+  end
   return instance
 end
 
 function TaskClass:done()
-  return false
+  if self.progress >= constants.max_progress then
+    return true
+  else
+    return false
+  end
 end
 
 function TaskClass:description()
   return nil
+end
+
+function TaskClass:update(dt)
+  print("Updating task", dt)
+  if self.type == 'serving' then
+    local customer, volunteer = map.occupant(self.locations[1]), map.occupant(self.locations[2])
+    if customer and volunteer and customer.type == 'customer' and customer:arrived() 
+      and volunteer:arrived() then
+      self.progress = self.progress + dt * constants.scale.work
+    end
+  elseif self.type == 'stocking' then
+  else
+    assert(false, string.format("Unhandled task type of %s", self.type))
+  end
 end
 
