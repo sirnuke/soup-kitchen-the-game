9a687fe0fb4e68ad6abc9096ca6f3edc7cd52602
Bryan DeGrendel <bryan@degrendel.com>
2014-03-29 02:36:29 -0400

    Updated behavior of the TaskClass
    1. Added a draw function.
    2. TaskClass now tracks the action, rather than a table of locations.

--- soup-kitchen/task.lua
+++ soup-kitchen/task.lua
@@ -5,24 +5,17 @@ TaskClass = {}
 TaskClass.__index = TaskClass
 TaskClass.types = { serving='serving', stock='stocking' }
 
-function TaskClass.new(type, locations, owner)
+function TaskClass.new(type, action, owner)
   assert(TaskClass.types[type])
-  assert(locations)
+  assert(action)
 
   local instance = {}
   setmetatable(instance, TaskClass)
   instance.type = type
-  instance.locations = locations
+  instance.action = action
   instance.progress = 0
   instance.owner = owner
   print("Creating task", type, owner)
-  if type == 'serving' then
-    assert(#locations == 2)
-    assert(owner)
-  elseif type == 'stocking' then
-  else
-    assert(false, string.format("Unhandled task type of %s", type))
-  end
   return instance
 end
 
@@ -40,7 +33,8 @@ end
 
 function TaskClass:update(dt)
   if self.type == 'serving' then
-    local customer, volunteer = map.occupant(self.locations[1]), map.occupant(self.locations[2])
+    local customer, volunteer = map.occupant(self.action.customer),
+      map.occupant(self.action.volunteer)
     if customer == self.owner and volunteer and customer:arrived() and volunteer:arrived() then
       self.progress = self.progress + dt * constants.scale.work
     end
@@ -51,5 +45,20 @@ function TaskClass:update(dt)
 end
 
 function TaskClass:draw()
+  if self.type == 'serving' then
+    local customer, volunteer = map.occupant(self.action.customer),
+      map.occupant(self.action.volunteer)
+    if not customer == self.owner then
+      love.graphics.draw(ActionClass.images.potential, self.action.screen.customer.x,
+        self.action.screen.customer.y)
+      love.graphics.draw(ActionClass.images.potential, self.action.screen.volunteer.x,
+        self.action.screen.volunteer.y)
+    elseif not volunteer or volunteer:arrived() then
+    else
+    end
+  elseif selt.type == 'stocking' then
+  else
+    assert(false, string.format("Unhandled task type of %s", self.type))
+  end
 end
 
