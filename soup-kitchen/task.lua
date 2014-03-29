d1fa7b5addf288e49ff46902a51989766d4f1e17
Bryan DeGrendel <bryan@degrendel.com>
2014-03-28 23:01:31 -0400

    Updated task's description behavior
    1. Description will be calculated based on the type and data of a task,
    rather than an upfront string.

--- soup-kitchen/task.lua
+++ soup-kitchen/task.lua
@@ -5,16 +5,22 @@ TaskClass = {}
 TaskClass.__index = TaskClass
 TaskClass.types = { serving='serving', stock='stocking' }
 
-function TaskClass.new(type, description, locations)
+function TaskClass.new(type, locations)
   assert(TaskClass.types[type])
-  assert(description)
   assert(locations)
 
   local instance = {}
   setmetatable(instance, TaskClass)
   instance.type = type
-  instance.description = description
   instance.locations = locations
   return instance
+end
+
+function TaskClass:done()
+  return false
+end
+
+function TaskClass:description()
+  return nil
 end
 
