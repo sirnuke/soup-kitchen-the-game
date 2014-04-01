1fcdd4164221458de8a4ef7c1b5e5400cb189df4
Bryan DeGrendel <bryan@degrendel.com>
2014-04-01 05:45:30 -0400

    Lots of refactoring to Pawn.
    1. Still needs lots of polishing to the pathfinding, however.

--- soup-kitchen/state/pawn.lua
+++ soup-kitchen/state/pawn.lua
@@ -3,85 +3,68 @@
 
 PawnClass = {}
 PawnClass.__index = PawnClass
-PawnClass.types = { player='player', employee='employee', volunteer='volunteer',
-  customer='customer' }
-PawnClass.images = { }
 
-function PawnClass.new(type, coord)
-  assert(PawnClass.types[type])
-  local image = nil
+function PawnClass.new(map, coord)
 
   if not PawnClass.pathfinding then
     PawnClass.pathfinding = { }
     PawnClass.pathfinding.map = { }
 
-    for y = 1,constants.sizes.map.h do
+    for y = 1,C.sizes.map.height do
       local row = { }
-      for x = 1,constants.sizes.map.w do
+      for x = 1,C.sizes.map.width do
         table.insert(row, nil)
       end
       table.insert(PawnClass.pathfinding.map, row)
     end
   end
 
-  if type == 'player' then
-    if not PawnClass.images.player then
-      PawnClass.images.player = love.graphics.newImage("images/pawns/player.png")
-    end
-    image = PawnClass.images.player
-  elseif type == 'employee' then
-  elseif type == 'volunteer' then
-  elseif type == 'customer' then
-    if not PawnClass.images.customer then
-      PawnClass.images.customer = love.graphics.newImage("images/pawns/customer.png")
-    end
-    image = PawnClass.images.customer
-  else
-    assert(false, string.format("Unhandled pawn type %s", type))
-  end
-
-
   local instance = {}
   setmetatable(instance, PawnClass)
-  instance.type = type
   instance.exited = false
   instance.task = false
+  instance.map = map
   if coord == 'enter' then
-    assert(not map.occupant(constants.coords.entrance))
-    instance.position = map.position(constants.coords.entrance)
-    instance.position.x = instance.position.x - constants.sizes.square
-    instance.coordinate = Coordinate.dup(constants.coords.entrance)
-    instance.path = { Coordinate.dup(constants.coords.entrance) }
-    instance.destination = map.position(constants.coords.entrance)
+    assert(not map:occupant(C.coords.entrance))
+    instance.position = C.coords.entrance:position()
+    instance.position.x = instance.position.x - C.sizes.square
+    instance.coordinate = C.coords.entrance:duplicate()
+    instance.path = { C.coords.entrance:duplicate() }
+    instance.destination = C.coords.entrance:position()
+    map:set_occupant(instance.coordinate, instance)
+  elseif coord then
+    assert(not map:blocked(coord) and not map:occupant(coord))
+    instance.position = coord:position()
+    instance.coordinate = coord:duplicate()
+    instance.path = nil
+    instance.destination = nil
+    map:set_occupant(instance.coordinate, instance)
   else
-    assert(not map.blocked(coord))
-    assert(not map.occupant(coord))
-    instance.position = map.position(coord)
-    instance.coordinate = Coordinate.new(coord.x, coord.y)
+    instance.position = nil
+    instance.coordinate = nil
     instance.path = nil
     instance.destination = nil
   end
-  map.set_occupant(instance.coordinate, instance)
-  inherits(GuiElementClass.new(instance.coordinate, constants.sizes.pawn, constants.sizes.pawn, 
-    { normal=image, hover=image, pressed=image }), instance)
+  instance.gui = InteractableClass.new(instance.coordinate, C.sizes.pawn, C.sizes.pawn)
   return instance
 end
 
 function PawnClass:draw()
-  love.graphics.draw(PawnClass.images[self.type], self.screen.x, self.screen.y)
+  -- TODO: This function will almost certainly be overridden in the future
+  assert(self.image, "Child didn't load an image!")
+  Screen:draw(self.image, self.screen)
 end
 
 function PawnClass:set_screen()
-  self.screen = {}
-  self.screen.x = self.position.x - (constants.sizes.square - constants.sizes.pawn) / 2
-  self.screen.y = self.position.y - (constants.sizes.square - constants.sizes.pawn) / 2
+  self.screen = self.coordinate:screen()
+  self.gui.set_point(self.screen)
 end
 
-function PawnClass:move(coord)
+function PawnClass:jump(coordinate)
   self.path = nil
   self.destination = nil
-  self.cordinates = Coordinate.new(coord.x, coord.y)
-  self.position = map.position(coord)
+  self.cordinate = coordinate:duplicate()
+  self.position = coordinate:position()
   self:set_screen()
 end
 
@@ -94,7 +77,7 @@ function PawnClass:arrived()
 end
 
 function PawnClass:pathfind(coord)
-  local current = map.coordinate(self.position.x, self.position.y)
+  local current = self.position:coordinate()
 
   PawnClass.pathfinding.map[current.y][current.x] = true
 
@@ -105,65 +88,65 @@ function PawnClass:pathfind(coord)
 
   while #queue > 0 and queue[1] ~= coord do
     --print("Getting neighbors of ", queue[1].x, queue[1].y)
-    for k,v in ipairs(map.get_neighbors(queue[1])) do
+    for k,v in ipairs(self.map:get_neighbors(queue[1])) do
       --print("Checking", v.x, v.y)
       if not PawnClass.pathfinding.map[v.y][v.x] then
-        PawnClass.pathfinding.map[v.y][v.x] = Coordinate.new(queue[1].x, queue[1].y) 
-        table.insert(queue, Coordinate.new(v.x, v.y))
+        PawnClass.pathfinding.map[v.y][v.x] = queue[1]:duplicate()
+        table.insert(queue, v:duplicate())
       end
     end
     table.remove(queue, 1)
   end
   assert(#queue > 0, "Couldn't find a path!")
   assert(queue[1] == coord, "Top of queue should be the end")
-  self.path = { Coordinate.new(coord.x, coord.y) }
+  self.path = { coord:duplicate() }
   local square = PawnClass.pathfinding.map[coord.y][coord.x]
   --print("Path ends up at ", x, y, type(square))
   while type(square) ~= "boolean" do
     assert(type(square) == "table")
-    table.insert(self.path, Coordinate.new(square.x, square.y))
+    table.insert(self.path, square:duplicate())
     --print("Looking up parent of ", square.x, square.y)
     square = PawnClass.pathfinding.map[square.y][square.x]
   end
-  for y = 1,constants.sizes.map.h do for x = 1,constants.sizes.map.w do
+  for y = 1,C.sizes.map.height do for x = 1,C.sizes.map.width do
     PawnClass.pathfinding.map[y][x] = nil
   end end
-  self.destination = map.position(self.path[#self.path])
+  self.destination = self.path[#self.path]:position()
 end
 
 function PawnClass:leave()
-  self:pathfind(constants.coords.exit)
-  map.set_occupant(self.coordinate, nil)
-  self.coordinate = Coordinate.dup(constants.coords.exit_off)
-  table.insert(self.path, 1, Coordinate.dup(constants.coords.exit_off))
+  self:pathfind(C.coords.exit)
+  self.map:set_occupant(self.coordinate, nil)
+  self.coordinate = C.coords.exit_off:duplicate()
+  table.insert(self.path, 1, C.coords.exit_off:duplicate())
 end
 
 function PawnClass:go(coord)
-  assert(not map.blocked(coord))
-  assert(not map.occupant(coord))
+  assert(not self.map:blocked(coord))
+  assert(not self.map:occupant(coord))
 
   self:pathfind(coord)
 
-  map.set_occupant(self.coordinate, nil)
-  map.set_occupant(coord, self)
-  self.coordinate = Coordinate.new(coord.x, coord.y)
+  self.map:set_occupant(self.coordinate, nil)
+  self.map:set_occupant(coord, self)
+  self.coordinate = coord:duplicate()
 end
 
 function PawnClass:update(dt)
   if self.path then
     assert(#self.path > 0 and self.destination)
-    local dx, dy = self.destination.x - self.position.x, self.destination.y - self.position.y
-    local distance = constants.scale.walk * dt
+    local dx, dy = self.destination - self.position
+    local distance = C.scale.walk * dt
     while distance > 0 do
-      if self.destination.x == self.position.x and self.destination.y == self.position.y then
+      if self.destination == self.position then
         table.remove(self.path)
         if #self.path > 0 then
-          self.destination = map.position(self.path[#self.path])
+          self.destination = self.path[#self.path]:position()
         else
           self.path = nil
           self.destination = nil
           distance = 0
-          if self.coordinate == constants.coords.exit_off then
+          if self.coordinate == C.coords.exit_off then
             print("Pawn has left the board")
             self.exited = true
           end
@@ -175,11 +158,9 @@ function PawnClass:update(dt)
 end
 
 function PawnClass:walk(distance)
-  if distance == 0 then
-    return 0
-  end
+  if distance == 0 then return 0 end
 
-  local dx, dy = self.destination.x - self.position.x, self.destination.y - self.position.y
+  local dx, dy = self.destination - self.position
   assert(dx == 0 or dy == 0)
   if math.abs(dx) + math.abs(dy) > distance then
     if dx ~= 0 then
@@ -205,14 +186,5 @@ function PawnClass:walk(distance)
   self.position.y = self.position.y + dy
   self:set_screen()
   return distance
-end
-
-function PawnClass:clicked(x, y)
-  if x >= self.screen.x and x < self.screen.x + constants.sizes.pawn and
-     y >= self.screen.y and y < self.screen.y + constants.sizes.pawn then
-     return true
-   else
-     return false
-   end
 end
 
