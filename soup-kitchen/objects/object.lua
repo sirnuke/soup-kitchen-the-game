2dfea1e58431639e85e5cf2ff0e4ae279fb15672
Bryan DeGrendel <bryan@degrendel.com>
2014-04-09 14:33:51 -0400

    Started to flesh out the object base class

--- soup-kitchen/objects/object.lua
+++ soup-kitchen/objects/object.lua
@@ -4,9 +4,22 @@
 ObjectClass = {}
 ObjectClass.__index = ObjectClass
 
-function ObjectClass.new(parent)
+function ObjectClass.new()
   local instance = {}
-  instance.parent = parent
+  instance.holder = nil
+  instance.location = nil
   return instance
+end
+
+function ObjectClass:grabbed(holder)
+  assert(not self.location or not self.holder and holder)
+  self.location = nil
+  self.holder = holder
+end
+
+function ObjectClass:dropped(location)
+  assert(not self.location or not self.holder and location)
+  self.location = location
+  self.holder = nil
 end
 
