b57319f7e40ea89aac602c9944fe256c5c05aec3
Bryan DeGrendel <bryan@degrendel.com>
2014-04-12 17:50:44 -0400

    Updated the base Task class

--- soup-kitchen/tasks/task.lua
+++ soup-kitchen/tasks/task.lua
@@ -3,70 +3,31 @@
 
 TaskClass = {}
 TaskClass.__index = TaskClass
-TaskClass.types = { serving='serving', stock='stocking' }
 
-function TaskClass.new(type, action, owner)
-  assert(TaskClass.types[type])
+function TaskClass.new(equipment, owner)
   assert(action)
 
   local instance = {}
   setmetatable(instance, TaskClass)
-  instance.type = type
-  instance.action = action
-  instance.progress = 0
+  instance.equipment = equipment
   instance.owner = owner
-  print("Creating task", type, owner)
   return instance
 end
 
 function TaskClass:done()
-  if self.progress >= constants.max_progress then
-    return true
-  else
-    return false
-  end
+  assert(false, "Task doesn't implement done()")
 end
 
+-- Defaults to 'nil' (no task list description)
 function TaskClass:description()
   return nil
 end
 
 function TaskClass:update(dt)
-  if self.type == 'serving' then
-    local customer, volunteer = map.occupant(self.action.customer),
-      map.occupant(self.action.volunteer)
-    if customer == self.owner and volunteer and customer:arrived() and volunteer:arrived() then
-      self.progress = self.progress + dt * constants.scale.work
-    end
-  elseif self.type == 'stocking' then
-  else
-    assert(false, string.format("Unhandled task type of %s", self.type))
-  end
+  assert(false, "Task doesn't implement update(dt)")
 end
 
 function TaskClass:draw()
-  if self.type == 'serving' then
-    local customer, volunteer = map.occupant(self.action.customer),
-      map.occupant(self.action.volunteer)
-    if not customer == self.owner then
-      love.graphics.draw(ActionClass.images.potential, self.action.screen.customer.x,
-        self.action.screen.customer.y)
-      love.graphics.draw(ActionClass.images.potential, self.action.screen.volunteer.x,
-        self.action.screen.volunteer.y)
-    elseif not volunteer or not volunteer:arrived() then
-      love.graphics.draw(ActionClass.images.potential, self.action.screen.customer.x,
-        self.action.screen.customer.y)
-      love.graphics.draw(ActionClass.images.halted, self.action.screen.volunteer.x,
-        self.action.screen.volunteer.y)
-    else
-      love.graphics.draw(ActionClass.images.active, self.action.screen.customer.x,
-        self.action.screen.customer.y)
-      love.graphics.draw(ActionClass.images.active, self.action.screen.volunteer.x,
-        self.action.screen.volunteer.y)
-    end
-  elseif selt.type == 'stocking' then
-  else
-    assert(false, string.format("Unhandled task type of %s", self.type))
-  end
+  assert(false, "Task doesn't implement draw()")
 end
 
