b38e5d0ae3b624b71dde0cdee4102dd8f9dc917c
Bryan DeGrendel <bryan@degrendel.com>
2014-04-13 03:48:50 -0400

    Implemented a simple Volunteer class
    1. Same as player, was intended to be just a PawnClass, not is a class
    extending PawnClass.

--- soup-kitchen/pawns/volunteer.lua
+++ soup-kitchen/pawns/volunteer.lua
@@ -1,3 +1,17 @@
 -- Soup Kitchen
 -- Bryan DeGrendel (c) 2014
 
+VolunteerClass = {}
+VolunteerClass.__index = VolunteerClass
+
+function VolunteerClass.new(map)
+  if not PlayerClass.image then
+    VolunteerClass.image = love.graphics.newImage("images/pawns/volunteer.png")
+  end
+
+  local instance = PawnClass.new(map, nil)
+  setmetatable(instance, VolunteerClass)
+  inherits(PawnClass, instance)
+  return instance
+end
+
