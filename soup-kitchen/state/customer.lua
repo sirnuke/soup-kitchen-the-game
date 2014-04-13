5f459138a629a81938186b6d0cc90c520b324761
Bryan DeGrendel <bryan@degrendel.com>
2014-04-12 22:01:16 -0400

    Start of a better Customer class
    1. Removed lots of old code.

--- soup-kitchen/state/customer.lua
+++ soup-kitchen/state/customer.lua
@@ -3,7 +3,6 @@
 
 CustomerClass = {}
 CustomerClass.__index = CustomerClass
-CustomerClass.states = { waiting='waiting', inline='inline', gotfood='gotfood', eating='eating' }
 CustomerClass.image = nil
 
 function CustomerClass.new(map, stage)
@@ -15,53 +14,22 @@ function CustomerClass.new(map, stage)
   setmetatable(instance, CustomerClass)
   inherits(PawnClass.new(map, nil), instance)
   instance.equipment = nil
-  instance.state = 'waiting'
   instance.stage = stage
+  instance.tray = nil
   instance.task = nil
   return instance
 end
 
 function CustomerClass:update(dt)
-  if self.state == 'waiting' then
-    if not map.occupant(constants.coords.entrance) then
-      self.equipment = map.equipment.dispensing.trays
-      self.task = GetFoodTaskClass.new(self.equipment, self)
-      table.insert(session.tasks, self.task)
-      self.state = 'inline'
+  self:move(dt)
+  if not self.task then
+    assert(not self:spawned())
+    if not map:occupied(C.coordinates.customer.entrance) then
+      self:enter(C.coordinates.customer.entrance)
+      self.task = GetFodTaskClass.new(map.equipment.dispensing.trays, self)
     end
-  elseif self.state == 'inline' then
-    self.pawn:update(dt)
-    if self.pawn.coordinate ~= self.action.customer then
-      if not map.occupant(self.action.customer) and self.pawn:arrived() then
-        self.pawn:go(self.action.customer)
-      end
-    elseif self.task:done() then
-      self.action = self.action:next(self.stage)
-      self.pawn.action = self.action
-      if self.action == 'done' then
-        self.pawn:leave()
-        self.state = 'gotfood'
-      else
-        self.task = TaskClass.new('serving', self.action, self.pawn)
-        table.insert(session.tasks, self.task)
-      end
-    end
-  elseif self.state == 'gotfood' then
-    self.pawn:update(dt)
-    if self.pawn.exited then
-      self.pawn = nil
-      self.state = 'eating'
-    end
-  elseif self.state == 'eating' then
-    -- update eating counter
-  else
-    assert(false, string.format("Unknown customer state of %s", self.state))
-  end
-end
-
-function CustomerClass:draw()
-  if self.pawn then
-    self.pawn:draw()
+  elseif self:arrived() then
+    self.task:update(dt)
   end
 end
 
