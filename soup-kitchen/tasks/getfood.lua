dc9578d988a4224c2884afebb34d12242afc83e3
Bryan DeGrendel <bryan@degrendel.com>
2014-04-13 03:47:03 -0400

    Fixed inheritance bug in GetFoodTask

--- soup-kitchen/tasks/getfood.lua
+++ soup-kitchen/tasks/getfood.lua
@@ -7,9 +7,9 @@ GetFoodTaskClass.__index = GetFoodTaskClass
 function GetFoodTaskClass.new(equipment, owner)
   assert(owner)
 
-  local instance = {}
+  local instance = TaskClass.new(equipment, owner)
   setmetatable(instance, GetFoodTaskClass)
-  inherits(TaskClass.new(equipment, owner), instance)
+  inherits(TaskClass, instance)
   return instance
 end
 
